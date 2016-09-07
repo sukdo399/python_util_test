0c8fffeb0c0aa2ce1714ef1677279f4260aa1507
Seokha Ko <sh.ko@lge.com>
2016-09-07 03:18:13 -0400

    ToDict Mixin & Json Mixin

---
+++ JsonMixin.py
@@ -0,0 +1,9 @@
+class JsonMixin(object):
+
+    @classmethod
+    def from_json(cls, data):
+        kwargs = json.loads(data)
+        return cls(**kwargs)
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
